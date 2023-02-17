13e03c9c1511c11f3dbe1320d4e5c0977fe61dc6
gian <giansingh4710@gmail.com>
2023-02-17 10:22:08 -0500

    added Bhai randhri Singh darbar Sahib and 5granthi Bhai jaswant singh ji

---
+++ PanjGranthi(BhaiJaswantSinghJi)/TRACKS.js
@@ -0,0 +1,7 @@
+const PanjGranthi = [
+  "http://45.41.235.33/~daassto1/audios/paath/PanjGranthiBhaiJaswantSinghJi/Asa Kee Vaar.mp3",
+  "http://45.41.235.33/~daassto1/audios/paath/PanjGranthiBhaiJaswantSinghJi/Bavan Akhri M5.mp3",
+  "http://45.41.235.33/~daassto1/audios/paath/PanjGranthiBhaiJaswantSinghJi/Dakhni Oankar.mp3",
+  "http://45.41.235.33/~daassto1/audios/paath/PanjGranthiBhaiJaswantSinghJi/Sidh Gosth.mp3",
+  "http://45.41.235.33/~daassto1/audios/paath/PanjGranthiBhaiJaswantSinghJi/Sukhmani Sahib.mp3",
+]
