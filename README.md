0bda8e6770ea17bada6c029cfa3c85204a515607
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2024-03-25 12:34:59 -0400

    track types when index track

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ I have made many websites that have audios of different things having to do with
 websites and more packaged into one. So if you wanna listen to scripture readings to singing(keertan) to discourse(katha). This site has it. Open to
 any suggestions on things to add or anything
 
-![Screenshot 2023-02-28 at 3 46 34 PM](https://user-images.githubusercontent.com/73843250/221975737-8cb22928-04d1-4c11-8efc-b702009dda93.jpg)
+<!-- ![Screenshot 2023-02-28 at 3 46 34 PM](https://user-images.githubusercontent.com/73843250/221975737-8cb22928-04d1-4c11-8efc-b702009dda93.jpg) -->
+![Screenshot 2024-03-21 at 10 17 57 AM](https://github.com/Giansingh4710/cs490_exercise_app_backend/assets/73843250/9baa7d1f-c95f-4b33-bf48-ae61dd97290d)
--- components/ListenPage/IndexTrackModal/index.js
+++ components/ListenPage/IndexTrackModal/index.js
@@ -1,9 +1,268 @@
 import ALL_THEMES from '@/utils/themes.js'
 import { Button, Modal } from '@mui/material'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ALL_SHABADS } from './allShabads.js'
 import CancelIcon from '@mui/icons-material/Cancel'
 import { containsOnlyDigits as digOnly } from '@/utils/helper_funcs.js'
+
+function convertToGurmukhi(input) {
+  const mapping = {
+    a: 'ੳ',
+    A: 'ਅ',
+    s: 'ਸ',
+    S: 'ਸ਼',
+    d: 'ਦ',
+    D: 'ਧ',
+    f: 'ਡ',
+    F: 'ਢ',
+    g: 'ਗ',
+    G: 'ਘ',
+    h: 'ਹ',
+    H: '੍ਹ',
+    j: 'ਜ',
+    J: 'ਝ',
+    k: 'ਕ',
+    K: 'ਖ',
+    l: 'ਲ',
+    L: 'ਲ਼',
+    q: 'ਤ',
+    Q: 'ਥ',
+    w: 'ਾ',
+    W: 'ਾਂ',
+    e: 'ੲ',
+    E: 'ਓ',
+    r: 'ਰ',
+    R: '੍ਰ',
+    '®': '੍ਰ',
+    t: 'ਟ',
+    T: 'ਠ',
+    y: 'ੇ',
+    Y: 'ੈ',
+    u: 'ੁ',
+    ü: 'ੁ',
+    U: 'ੂ',
+    '¨': 'ੂ',
+    i: 'ਿ',
+    I: 'ੀ',
+    o: 'ੋ',
+    O: 'ੌ',
+    p: 'ਪ',
+    P: 'ਫ',
+    z: 'ਜ਼',
+    Z: 'ਗ਼',
+    x: 'ਣ',
+    X: 'ਯ',
+    c: 'ਚ',
+    C: 'ਛ',
+    v: 'ਵ',
+    V: 'ੜ',
+    b: 'ਬ',
+    B: 'ਭ',
+    n: 'ਨ',
+    ƒ: 'ਨੂੰ',
+    N: 'ਂ',
+    ˆ: 'ਂ',
+    m: 'ਮ',
+    M: 'ੰ',
+    µ: 'ੰ',
+    '`': 'ੱ',
+    '~': 'ੱ',
+    '¤': 'ੱ',
+    Í: '੍ਵ',
+    ç: '੍ਚ',
+    '†': '੍ਟ',
+    œ: '੍ਤ',
+    '˜': '੍ਨ',
+    '´': 'ੵ',
+    Ï: 'ੵ',
+    æ: '਼',
+    Î: '੍ਯ',
+    ì: 'ਯ',
+    í: '੍ਯ',
+    // 1: '੧',
+    // 2: '੨',
+    // 3: '੩',
+    // 4: '੪',
+    // 5: '੫',
+    // 6: '੬',
+    // '^': 'ਖ਼',
+    // 7: '੭',
+    // '&': 'ਫ਼',
+    // 8: '੮',
+    // 9: '੯',
+    // 0: '੦',
+    '\\': 'ਞ',
+    '|': 'ਙ',
+    '[': '।',
+    ']': '॥',
+    '<': 'ੴ',
+    '¡': 'ੴ',
+    Å: 'ੴ',
+    Ú: 'ਃ',
+    Ç: '☬',
+    '@': 'ੑ',
+    '‚': '❁',
+    '•': '੶',
+    ' ': ' ',
+  }
+  const gurmukhi_input = input
+    .split('')
+    .map((char) => mapping[char] || char)
+    .join('')
+  return gurmukhi_input
+}
+
+function get_sbds_first_letters(gurmukhi_input) {
+  function first_letters_gurmukhi(words) {
+    if (typeof words !== 'string') return words
+
+    let newWords = words
+
+    const reverseMapping = {
+      ਉ: 'ੳ',
+      ਊ: 'ੳ',
+      ਆ: 'ਅ',
+      ਆਂ: 'ਅ',
+      ਐ: 'ਅ',
+      ਔ: 'ਅ',
+      ਇ: 'ੲ',
+      ਈ: 'ੲ',
+      ਏ: 'ੲ',
+      // 'ੋੁ': 'uo',
+    }
+
+    const simplifications = [
+      ['E', 'a'],
+      ['ਓ', 'ੳ'],
+      ['L', 'l'],
+      ['ਲ਼', 'ਲ'],
+      ['S', 's'],
+      ['ਸ਼', 'ਸ'],
+      ['z', 'j'],
+      ['ਜ਼', 'ਜ'],
+      ['Z', 'g'],
+      ['ਗ਼', 'ਗ'],
+      ['\\^', 'K'],
+      ['ਖ਼', 'ਖ'],
+      ['ƒ', 'n'],
+      ['ਨੂੰ', 'ਨ'],
+      ['&', 'P'],
+      ['ਫ਼', 'ਫ'],
+    ]
+    simplifications.forEach((e) => {
+      newWords = newWords.replace(new RegExp(e[0], 'g'), e[1])
+    })
+
+    newWords = newWords
+      .replace(/\]/g, '')
+      .replace(/\[/g, '')
+      .replace(/॥/g, '')
+      .replace(/।/g, '')
+      .replace(/rhwau dUjw/g, '')
+      .replace(/rhwau/g, '')
+      .replace(/[0-9]/g, '')
+      .replace(/[;,.]/g, '')
+
+    function firstLetter(word) {
+      let letter = word[0]
+      if (letter in reverseMapping) {
+        letter = reverseMapping[letter]
+      }
+      return letter
+    }
+
+    const letters = newWords.split(' ').map(firstLetter).join('')
+    return letters
+  }
+
+  for (let i = 0; i < gurmukhi_input.length; i++) {
+    const char = gurmukhi_input[i]
+    if (char >= '0' && char <= '9') {
+      return []
+    }
+  }
+
+  const all_matched_shabad_keys = []
+  for (const key in ALL_SHABADS) {
+    const shabadArray = ALL_SHABADS[key]
+
+    for (let pu_ln_idx = 0; pu_ln_idx < shabadArray.length; pu_ln_idx += 3) {
+      const line = shabadArray[pu_ln_idx]
+      const first_letters = first_letters_gurmukhi(line)
+
+      let line_matched = true
+      for (let i = 0; i < gurmukhi_input.length; i++) {
+        if (
+          first_letters.length === i ||
+          first_letters[i] !== gurmukhi_input[i]
+        ) {
+          line_matched = false
+          break
+        }
+      }
+
+      if (line_matched) {
+        all_matched_shabad_keys.push({
+          shabadId: key,
+          lineInd: pu_ln_idx,
+        })
+        break
+      }
+    }
+  }
+  return all_matched_shabad_keys
+}
+
+const styles = {
+  main_btn: {
+    margin: '2em',
+    borderRadius: '10px',
+    color: ALL_THEMES.theme1.text2,
+    backgroundColor: ALL_THEMES.theme1.third,
+  },
+  cont: {
+    padding: '2em',
+    borderRadius: '1em',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '70vw',
+    backgroundColor: '#ff7f50',
+    color: ALL_THEMES.theme1.text1,
+  },
+  closeModalBtn: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    fontSize: '31px',
+    fontWeight: 'bold',
+    margin: '-1em',
+    color: ALL_THEMES.theme1.text2,
+  },
+  userInputItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '10px',
+    marginBottom: '5px',
+    padding: '10px',
+    backgroundColor: '#0077be',
+  },
+  label: {
+    flex: 0.5,
+    fontWeight: 500,
+    letterSpacing: 0.2,
+    fontSize: '0.75rem',
+  },
+  userDesc: {
+    flex: 1,
+  },
+  timeInput: {
+    width: '3em',
+    color: ALL_THEMES.theme1.text1,
+  },
+}
 
 export default function IndexTrackBtnAndModal({ artist, link, saveTrackLS }) {
   const [modalOpen, setModal] = useState(false)
@@ -12,6 +271,7 @@ export default function IndexTrackBtnAndModal({ artist, link, saveTrackLS }) {
   const [shabads, setShabads] = useState([])
   const [lineClicked, setLineClicked] = useState('')
   const [submitFormBtnDisabled, setSubmitFormBtnDisabled] = useState(false)
+  const [theTrackType, setTrackType] = useState('')
   const formData = useRef(null)
 
   const [timestamp, setTimestamp] = useState({
@@ -19,6 +279,18 @@ export default function IndexTrackBtnAndModal({ artist, link, saveTrackLS }) {
     minutes: '',
     seconds: '',
   })
+
+  useEffect(() => {
+    function getTrackType() {
+      const trackType = localStorage.getItem('IndexTrack trackType')
+      if (trackType === null) {
+        return 'random'
+      }
+      return trackType
+    }
+
+    setTrackType(getTrackType())
+  }, [])
 
   function formValidation(e) {
     e.preventDefault()
@@ -49,13 +321,17 @@ export default function IndexTrackBtnAndModal({ artist, link, saveTrackLS }) {
       return
     }
 
-    if ( digOnly(timestamp.hours) && digOnly(timestamp.minutes) && digOnly(timestamp.seconds)) {
+    if (
+      digOnly(timestamp.hours) &&
+      digOnly(timestamp.minutes) &&
+      digOnly(timestamp.seconds)
+    ) {
       alert('Timestamp cannot be empty')
       return
     }
 
     add_to_form_to_send_to_server('linkToGoTo', window.location.href) //come back to this page
-    add_to_form_to_send_to_server('keertani', artist)
+    add_to_form_to_send_to_server('artist', artist)
     add_to_form_to_send_to_server('link', link)
 
     saveTrackLS()
@@ -258,6 +534,24 @@ export default function IndexTrackBtnAndModal({ artist, link, saveTrackLS }) {
                 }
               />
             </div>
+            <div>
+              <label style={styles.label} htmlFor='trackType'>
+                Type of Track:
+              </label>
+              <select
+                name='trackType'
+                id='trackType'
+                value={theTrackType}
+                defaultValue={theTrackType}
+                onChange={(e) => {
+                  localStorage.setItem('IndexTrack trackType', e.target.value)
+                  setTrackType(e.target.value)
+                }}
+              >
+                <option value='random'>random</option>
+                <option value='SDO_MGA_1'>SDO_MGA_1</option>
+              </select>
+            </div>
             <button onClick={() => setModal(false)}>Close</button>
             <button type='submit' disabled={submitFormBtnDisabled}>
               Add
@@ -267,264 +561,4 @@ export default function IndexTrackBtnAndModal({ artist, link, saveTrackLS }) {
       </Modal>
     </div>
   )
-}
-
-function convertToGurmukhi(input) {
-  const mapping = {
-    a: 'ੳ',
-    A: 'ਅ',
-    s: 'ਸ',
-    S: 'ਸ਼',
-    d: 'ਦ',
-    D: 'ਧ',
-    f: 'ਡ',
-    F: 'ਢ',
-    g: 'ਗ',
-    G: 'ਘ',
-    h: 'ਹ',
-    H: '੍ਹ',
-    j: 'ਜ',
-    J: 'ਝ',
-    k: 'ਕ',
-    K: 'ਖ',
-    l: 'ਲ',
-    L: 'ਲ਼',
-    q: 'ਤ',
-    Q: 'ਥ',
-    w: 'ਾ',
-    W: 'ਾਂ',
-    e: 'ੲ',
-    E: 'ਓ',
-    r: 'ਰ',
-    R: '੍ਰ',
-    '®': '੍ਰ',
-    t: 'ਟ',
-    T: 'ਠ',
-    y: 'ੇ',
-    Y: 'ੈ',
-    u: 'ੁ',
-    ü: 'ੁ',
-    U: 'ੂ',
-    '¨': 'ੂ',
-    i: 'ਿ',
-    I: 'ੀ',
-    o: 'ੋ',
-    O: 'ੌ',
-    p: 'ਪ',
-    P: 'ਫ',
-    z: 'ਜ਼',
-    Z: 'ਗ਼',
-    x: 'ਣ',
-    X: 'ਯ',
-    c: 'ਚ',
-    C: 'ਛ',
-    v: 'ਵ',
-    V: 'ੜ',
-    b: 'ਬ',
-    B: 'ਭ',
-    n: 'ਨ',
-    ƒ: 'ਨੂੰ',
-    N: 'ਂ',
-    ˆ: 'ਂ',
-    m: 'ਮ',
-    M: 'ੰ',
-    µ: 'ੰ',
-    '`': 'ੱ',
-    '~': 'ੱ',
-    '¤': 'ੱ',
-    Í: '੍ਵ',
-    ç: '੍ਚ',
-    '†': '੍ਟ',
-    œ: '੍ਤ',
-    '˜': '੍ਨ',
-    '´': 'ੵ',
-    Ï: 'ੵ',
-    æ: '਼',
-    Î: '੍ਯ',
-    ì: 'ਯ',
-    í: '੍ਯ',
-    // 1: '੧',
-    // 2: '੨',
-    // 3: '੩',
-    // 4: '੪',
-    // 5: '੫',
-    // 6: '੬',
-    // '^': 'ਖ਼',
-    // 7: '੭',
-    // '&': 'ਫ਼',
-    // 8: '੮',
-    // 9: '੯',
-    // 0: '੦',
-    '\\': 'ਞ',
-    '|': 'ਙ',
-    '[': '।',
-    ']': '॥',
-    '<': 'ੴ',
-    '¡': 'ੴ',
-    Å: 'ੴ',
-    Ú: 'ਃ',
-    Ç: '☬',
-    '@': 'ੑ',
-    '‚': '❁',
-    '•': '੶',
-    ' ': ' ',
-  }
-  const gurmukhi_input = input
-    .split('')
-    .map((char) => mapping[char] || char)
-    .join('')
-  return gurmukhi_input
-}
-
-function get_sbds_first_letters(gurmukhi_input) {
-  function first_letters_gurmukhi(words) {
-    if (typeof words !== 'string') return words
-
-    let newWords = words
-
-    const reverseMapping = {
-      ਉ: 'ੳ',
-      ਊ: 'ੳ',
-      ਆ: 'ਅ',
-      ਆਂ: 'ਅ',
-      ਐ: 'ਅ',
-      ਔ: 'ਅ',
-      ਇ: 'ੲ',
-      ਈ: 'ੲ',
-      ਏ: 'ੲ',
-      // 'ੋੁ': 'uo',
-    }
-
-    const simplifications = [
-      ['E', 'a'],
-      ['ਓ', 'ੳ'],
-      ['L', 'l'],
-      ['ਲ਼', 'ਲ'],
-      ['S', 's'],
-      ['ਸ਼', 'ਸ'],
-      ['z', 'j'],
-      ['ਜ਼', 'ਜ'],
-      ['Z', 'g'],
-      ['ਗ਼', 'ਗ'],
-      ['\\^', 'K'],
-      ['ਖ਼', 'ਖ'],
-      ['ƒ', 'n'],
-      ['ਨੂੰ', 'ਨ'],
-      ['&', 'P'],
-      ['ਫ਼', 'ਫ'],
-    ]
-    simplifications.forEach((e) => {
-      newWords = newWords.replace(new RegExp(e[0], 'g'), e[1])
-    })
-
-    newWords = newWords
-      .replace(/\]/g, '')
-      .replace(/\[/g, '')
-      .replace(/॥/g, '')
-      .replace(/।/g, '')
-      .replace(/rhwau dUjw/g, '')
-      .replace(/rhwau/g, '')
-      .replace(/[0-9]/g, '')
-      .replace(/[;,.]/g, '')
-
-    function firstLetter(word) {
-      let letter = word[0]
-      if (letter in reverseMapping) {
-        letter = reverseMapping[letter]
-      }
-      return letter
-    }
-
-    const letters = newWords.split(' ').map(firstLetter).join('')
-    return letters
-  }
-
-  for (let i = 0; i < gurmukhi_input.length; i++) {
-    const char = gurmukhi_input[i]
-    if (char >= '0' && char <= '9') {
-      console.log('num in search input')
-      return []
-    }
-  }
-
-  const all_matched_shabad_keys = []
-  for (const key in ALL_SHABADS) {
-    const shabadArray = ALL_SHABADS[key]
-
-    for (let pu_ln_idx = 0; pu_ln_idx < shabadArray.length; pu_ln_idx += 3) {
-      const line = shabadArray[pu_ln_idx]
-      const first_letters = first_letters_gurmukhi(line)
-
-      let line_matched = true
-      for (let i = 0; i < gurmukhi_input.length; i++) {
-        if (
-          first_letters.length === i ||
-          first_letters[i] !== gurmukhi_input[i]
-        ) {
-          line_matched = false
-          break
-        }
-      }
-
-      if (line_matched) {
-        all_matched_shabad_keys.push({
-          shabadId: key,
-          lineInd: pu_ln_idx,
-        })
-        break
-      }
-    }
-  }
-  return all_matched_shabad_keys
-}
-
-const styles = {
-  main_btn: {
-    margin: '2em',
-    borderRadius: '10px',
-    color: ALL_THEMES.theme1.text2,
-    backgroundColor: ALL_THEMES.theme1.third,
-  },
-  cont: {
-    padding: '2em',
-    borderRadius: '1em',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '70vw',
-    backgroundColor: '#ff7f50',
-    color: ALL_THEMES.theme1.text1,
-  },
-  closeModalBtn: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    fontSize: '31px',
-    fontWeight: 'bold',
-    margin: '-1em',
-    color: ALL_THEMES.theme1.text2,
-  },
-  userInputItem: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '10px',
-    marginBottom: '5px',
-    padding: '10px',
-    backgroundColor: '#0077be',
-  },
-  label: {
-    flex: 0.5,
-    fontWeight: 500,
-    letterSpacing: 0.2,
-    fontSize: '0.75rem',
-  },
-  userDesc: {
-    flex: 1,
-  },
-  timeInput: {
-    width: '3em',
-    color: ALL_THEMES.theme1.text1,
-  },
 }
