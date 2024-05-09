7c02576bf1d1caa068661ffc9e59e3c09c64df35
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2024-05-09 12:55:40 -0400

    added Rimmy radio

---
+++ app/RimmyRadio/page.js
@@ -0,0 +1,6 @@
+import { ALL_OPTS } from './TRACKS.js'
+import ListenPage from '@/components/ListenPage/index.js'
+
+export default function akj() {
+  return <ListenPage title='Welcome to Rimmy Radio' tracksObj={ALL_OPTS} />
+}
